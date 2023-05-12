8d52f54b4419227acb90f516729801d670a7e63a
Muhammed Adel <muhammed.adel.elshall@gmail.com>
2023-05-12 18:25:56 +0300

    solve Carousel

--- src/components/pages/user/components/Carousel.js
+++ src/components/pages/user/components/Carousel.js
@@ -1,8 +1,14 @@
 import React from "react";
 import 'flowbite';
 // import '../../../../../public/images/'
-// import '../styles/carousel.css'
+import '../styles/carousel.css'
+import photo1 from '../../../assets/images/photo1.jpg';
+import photo2 from '../../../assets/images/photo2.jpg';
+import photo3 from '../../../assets/images/photo3.jpg';
+import photo4 from '../../../assets/images/photo4.jpg';
+import photo5 from '../../../assets/images/photo5.webp';
 function Carousel(){
+    // console.log(images)
     return(
         
 <div id="default-carousel" className="relative w-full" data-carousel="slide">
@@ -11,27 +17,27 @@ function Carousel(){
 
         <div  id='img1' className="hidden duration-700 ease-in-out" data-carousel-item>
         <h1 class="absolute block top-1/2 left-1/2 font-bold  -translate-x-1/2 -translate-y-1/2 sm:text-3xl ">Welcome</h1>
-            <img   className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
+            <img src={photo1}  className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
 
 
         </div>
-        <div id='img2'   className="hidden duration-700 ease-in-out" data-carousel-item>
+        <div id='img2' className="hidden duration-700 ease-in-out" data-carousel-item>
         <h1 class="absolute block top-1/2 left-1/2 font-bold  -translate-x-1/2 -translate-y-1/2 sm:text-3xl ">IN</h1>
-            <img className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
+            <img src={photo2} alt="photo1" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
         </div>
 
         <div id="img3"className="hidden duration-700 ease-in-out" data-carousel-item>
         <h1 class="absolute block top-1/2 left-1/2 font-bold  -translate-x-1/2 -translate-y-1/2 sm:text-3xl ">Our</h1>
-            <img  className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
+            <img src={photo3} alt="photo1" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
         </div>
 
         <div id="img4" className="hidden duration-700 ease-in-out" data-carousel-item>
         <h1 class="absolute block top-1/2 left-1/2 font-bold  -translate-x-1/2 -translate-y-1/2 sm:text-3xl ">Ecommerce</h1>
-            <img  className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
+            <img src={photo4} alt="photo1" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
         </div>
         <div id="img5" className="hidden duration-700 ease-in-out" data-carousel-item>
         <h1 class="absolute block top-1/2 left-1/2 font-bold  -translate-x-1/2 -translate-y-1/2 sm:text-3xl ">Website</h1>
-            <img  className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
+            <img src={photo5} alt="photo1" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" />
         </div>
     </div>
     <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">

175b7880d5c20877a63d750dd8ee9635d0537ffa
MustafaRamadan1 <Mustafa.Ramadan165@gmail.com>
2023-05-13 19:03:38 +0300

    Create ShowCategories Component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,10 +4,12 @@ import Footer from "../common/footer";
 import Carousel from "./user/components/Carousel";
 import TrendingProducts from './user/components/TrendingProducts';
 import About from './user/components/About';
+import ShowCategories from "./ecommerce/ShowCategories";
 function Home() {
   return (
     <div className="home">
     <Carousel/>
+    <ShowCategories/>
     <TrendingProducts/>
     <About/>
     </div>

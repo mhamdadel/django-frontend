f34f731639f7665ac94dacee2c3be6b4fc4c262e
Muhammed Adel <muhammed.adel.elshall@gmail.com>
2023-05-20 21:33:59 +0300

    delete Unimportant and unused compnents and add some futures

--- src/components/pages/user/MyOrders.js
+++ src/components/pages/user/MyOrders.js
@@ -19,9 +19,7 @@ function MyOrders() {
                 withCredentials: true,
             })
             .then((res) => {
-                setOrders(res.data);
-
-                console.log(res.data[0]);
+                handleSort(res.data)
                 setIsLoading(false);
             })
             .catch((err) => console.log(err));
@@ -43,7 +41,10 @@ function MyOrders() {
             return sum + (current.price * current.quantity);
         }, 0);
     };
-
+    function handleSort(orders) {
+        const sortedOrders = [...orders].sort((a, b) => a.order_id - b.order_id);
+        setOrders(sortedOrders);
+      }
     function updateOrder(id) {
         setOrders((prevOrders) =>
           prevOrders.map((order) =>
@@ -193,58 +194,6 @@ function MyOrders() {
                                                                 Cancel
                                                             </button>
                                                         )}
-                                                    </td>
-                                                    <td className="px-4 py-3 flex items-center justify-end">
-                                                        <button
-                                                            id="apple-imac-27-dropdown-button"
-                                                            data-dropdown-toggle="apple-imac-27-dropdown"
-                                                            className="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
-                                                            type="button"
-                                                        >
-                                                            <svg
-                                                                className="w-5 h-5"
-                                                                aria-hidden="true"
-                                                                fill="currentColor"
-                                                                viewBox="0 0 20 20"
-                                                                xmlns="http://www.w3.org/2000/svg"
-                                                            >
-                                                                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
-                                                            </svg>
-                                                        </button>
-                                                        <div
-                                                            id="apple-imac-27-dropdown"
-                                                            className="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
-                                                        >
-                                                            <ul
-                                                                className="py-1 text-sm text-gray-700 dark:text-gray-200"
-                                                                aria-labelledby="apple-imac-27-dropdown-button"
-                                                            >
-                                                                <li>
-                                                                    <a
-                                                                        href="#"
-                                                                        className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                                                    >
-                                                                        Show
-                                                                    </a>
-                                                                </li>
-                                                                <li>
-                                                                    <a
-                                                                        href="#"
-                                                                        className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                                                    >
-                                                                        Edit
-                                                                    </a>
-                                                                </li>
-                                                            </ul>
-                                                            <div className="py-1">
-                                                                <a
-                                                                    href="#"
-                                                                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                                                                >
-                                                                    Delete
-                                                                </a>
-                                                            </div>
-                                                        </div>
                                                     </td>
                                                 </tr>
                                             );

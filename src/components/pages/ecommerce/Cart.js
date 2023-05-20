a6d937db20c268c65e3abb0aec1577131ed959a7
merehanyehia <merehan0yehia@gmail.com>
2023-05-20 19:12:02 +0300

    changes made in order modal

--- src/components/pages/ecommerce/Cart.js
+++ src/components/pages/ecommerce/Cart.js
@@ -47,7 +47,7 @@ function Cart() {
         try {
             const cartData = create_order();
             console.log(transactionsData);
-            if (transactionsData.status = "COMPLETED") {
+            if (transactionsData.status === "COMPLETED") {
                 await axios.post(
                     "http://localhost:8000/orders/add_order/",
                     {
@@ -214,7 +214,7 @@ function Cart() {
       </div>
     </div>
     <center>
-    <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" className="orderCart block text-white   focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  " type="button" onClick={() => {
+    <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" className="orderCart block text-white   focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="button" onClick={() => {
       setShow(true)
     }} >Complete Your Order</button>
     </center>
@@ -223,9 +223,11 @@ function Cart() {
 <div id="authentication-modal" tabindex="-1" aria-hidden="true" className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div className="relative w-full max-w-md max-h-full">
         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="authentication-modal">
+            <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="authentication-modal" onClick={() => {
+      setShow(false)
+    }}>
                 <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                <span className="sr-only">Close modal</span>
+                <span className="sr-only" >Close modal</span>
             </button>
             <div className="px-6 py-6 lg:px-8">
                 <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">complete your data please</h3>
@@ -242,7 +244,7 @@ function Cart() {
                     <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >Submit</button>
 
                 </form>
-           
+
             </div>
         </div>
     </div>
